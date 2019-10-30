787910b8e6bf810b8515789673c31fd3ca45c60e
azurefoundry <boshcfaws@gmail.com>
2019-10-30 13:10:31 +0900

    first commit

---
+++ 01-docker-build.sh
@@ -0,0 +1,8 @@
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-Darwin-x86_64" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose up -d --build
+
+docker-compose run test-service -T
