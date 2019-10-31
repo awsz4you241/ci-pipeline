679430f6af3d2dde2c256a0e11d70a894d8316bd
azurefoundry <boshcfaws@gmail.com>
2019-10-31 10:46:11 +0900

    a

--- 01-docker-build.sh
+++ 01-docker-build.sh
@@ -1,9 +1,3 @@
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-Darwin-x86_64" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-cd ci-docker
-docker-compose up -d --build
-
-docker-compose run test-service -T
+#!/bin/sh
+docker run --privileged --name dind -d docker:dind
+docker ps
